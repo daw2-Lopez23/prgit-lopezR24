6463f7c4c1ee47276dc8c649a6baff7053d918e7
daw2-Lopez23 <lopezzunigaramon@fpllefia.com>
2023-09-26 17:14:21 +0200

    prgit-lopezR24 v1

---
+++ mitja.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+	float suma=0;
+	float mitja;
+	float nou_valor;
+	int numero_repeticions=0;
+	char consulta='s';
+	
+	while(consulta=='s') {
+		printf("LopezR24 Dòna la següent dada: ");
+		scanf("%f",&nou_valor);
+		suma=suma+nou_valor;
+		numero_repeticions=numero_repeticions+1;
+		printf("LopezR24 Vols continuar (s/n): ");
+		scanf(" %c",&consulta); 
+	}
+	mitja=suma/numero_repeticions;
+	printf("LopezR24 La mitja és: %.2f\n",mitja);	
+	return 0;
+}
+
